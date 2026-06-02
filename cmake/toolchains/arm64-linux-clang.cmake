ad85ea7588d3794484da4c3ff17d73a2765d5304
dev0 <dev0@iasoft.dev>
2026-06-03 01:19:24 +0530

    [WORKFLOW]: CI Fix

--- cmake/toolchains/arm64-linux-clang.cmake
+++ cmake/toolchains/arm64-linux-clang.cmake
@@ -14,25 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(triple aarch64-linux-gnu)
-set(CMAKE_C_COMPILER_TARGET   ${triple})
-set(CMAKE_CXX_COMPILER_TARGET ${triple})
+set(_auxid_triple aarch64-linux-gnu)
+set(CMAKE_C_COMPILER_TARGET ${_auxid_triple})
+set(CMAKE_CXX_COMPILER_TARGET ${_auxid_triple})
 
-set(CMAKE_SYSROOT /usr/aarch64-linux-gnu/sys-root)
-set(CMAKE_LIBRARY_ARCHITECTURE aarch64-linux-gnu)
+set(CMAKE_SYSROOT "/usr/${_auxid_triple}")
+set(CMAKE_FIND_ROOT_PATH "${CMAKE_SYSROOT}")
+set(CMAKE_LIBRARY_ARCHITECTURE ${_auxid_triple})
 
-string(APPEND CMAKE_C_FLAGS " -march=armv8-a+simd")
-string(APPEND CMAKE_CXX_FLAGS " -march=armv8-a+simd")
+string(APPEND CMAKE_C_FLAGS " -march=armv8-a+simd --sysroot=${CMAKE_SYSROOT} --gcc-toolchain=/usr")
+string(APPEND CMAKE_CXX_FLAGS " -march=armv8-a+simd --sysroot=${CMAKE_SYSROOT} --gcc-toolchain=/usr")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
-link_directories(${CMAKE_SYSROOT}/usr/lib64)
-link_directories(${CMAKE_SYSROOT}/lib64)
-
